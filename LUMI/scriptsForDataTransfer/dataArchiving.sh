0424d47c08059641e4ec3a6a6c878048dd60c362
Sebastian Wingbermuehle <sebastian.wingbermuehle@gmail.com>
2024-02-10 21:06:49 +0100

    Fix 1 typo

--- LUMI/scriptsForDataTransfer/dataArchiving.sh
+++ LUMI/scriptsForDataTransfer/dataArchiving.sh
@@ -15,7 +15,7 @@ PATH_FILES_TO_ARCHIVE=${ROOT_PATH}/inputFiles
 PATH_OUTPUT=${ROOT_PATH}/archivedSystems
 PATH_TRANSFER_METADATA=${ROOT_PATH}/transferredSystems
 
-PDBids=($(cat ${PATH_TRANSFER_METADATA}/transferredToLumi.txt) # $(cat ${PATH_TRANSFER_METADATA}/transferredToMeluXina.txt))
+PDBids=($(cat ${PATH_TRANSFER_METADATA}/transferredToLumi.txt)) # $(cat ${PATH_TRANSFER_METADATA}/transferredToMeluXina.txt))
 
 cwd=/scratch
 
